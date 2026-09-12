7cc4ba0e4a6d286f0b53f3b0f08fc1974205c2dc
kevinjaffe <kevinjaffe@hotmail.com>
2026-09-11 23:28:34 -0300

    Admins incluidos en la base de datos

--- backend/database/migrations/farmayopin_db.sql
+++ backend/database/migrations/farmayopin_db.sql
@@ -51,10 +51,7 @@ CREATE TABLE `compras` (
 -- Volcado de datos para la tabla `compras`
 --
 
-INSERT INTO `compras` (`id`, `usuario_id`, `total`, `fecha`) VALUES
-(1, 3, 2400.00, '2026-09-09 22:27:38'),
-(2, 3, 420.00, '2026-09-10 23:42:44'),
-(3, 3, 1704.00, '2026-09-10 23:50:21');
+-- (sin datos)
 
 -- --------------------------------------------------------
 
@@ -75,12 +72,7 @@ CREATE TABLE `compra_items` (
 -- Volcado de datos para la tabla `compra_items`
 --
 
-INSERT INTO `compra_items` (`id`, `compra_id`, `producto_id`, `cantidad`, `precio_unitario`, `subtotal`) VALUES
-(1, 1, 4, 4, 390.00, 1560.00),
-(2, 1, 5, 2, 420.00, 840.00),
-(3, 2, 5, 1, 420.00, 420.00),
-(4, 3, 5, 4, 420.00, 1680.00),
-(5, 3, 6, 2, 12.00, 24.00);
+-- (sin datos)
 
 -- --------------------------------------------------------
 
@@ -119,14 +111,7 @@ CREATE TABLE `productos` (
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `nombre`, `precio`, `detalle`, `foto`, `stock`, `creado_en`) VALUES
-(1, 'Paracetamol 500mg', 180.00, 'Caja de 20 comprimidos', NULL, 50, '2026-09-07 23:32:49'),
-(2, 'Ibuprofeno 400mg', 220.00, 'Caja de 10 capsulas blandas', NULL, 30, '2026-09-07 23:32:49'),
-(3, 'Dermaglos Crema', 650.00, 'Hidratacion profunda 100g', NULL, 20, '2026-09-07 23:32:49'),
-(4, 'Vitamina C 1g', 390.00, '10 tabletas efervescentes de calidad', NULL, 16, '2026-09-07 23:32:49'),
-(5, 'su madre', 420.00, 'la mejor madre', 'prod_20260907_234547_29c1c3c2.jpg', 62, '2026-09-07 23:45:47'),
-(6, 'lolll', 16.00, 'jasjjasaaaaa', 'prod_20260909_223111_f49e1b81.jpg', 20, '2026-09-09 22:31:11'),
-(7, 'lol3', 58.00, 'jaja', 'prod_20260910_235128_fdfe905f.jpg', 131, '2026-09-10 23:51:28');
+-- (sin datos)
 
 -- --------------------------------------------------------
 
@@ -148,11 +133,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `rol`, `creado_en`) VALUES
-(1, 'kevin chafe', 'kevinsacamedeaquiplis@gmail.com', '$2y$10$EpLZSeeMKs334tznMP8nE.Tm1COFJfFYFjC0mYBc24LyACwNwkrXG', 'cliente', '2026-09-06 21:12:52'),
-(2, 'facundo salad berry', 'alexismenchaca2005@gmail.com', '$2y$10$7DLPBV68TNehPla4rcPmPeH/Z9.YnE6XHZ5lZObrMSCNEMwI6YoWy', 'cliente', '2026-09-06 21:50:37'),
-(3, 'alexis', 'alexis@gmail.com', '$2y$10$a9zTSg4HFdnHdKt/DsFT9O9gnFSnu6SNXyI80uEZOjs/CCIU9NN56', 'cliente', '2026-09-06 23:02:59'),
-(4, 'kevin', 'kevin@gmail.com', '$2y$10$TMbqKmdoMMZUqymCrUkndud9cLeCEVFzmqQJRlIfOm12V0LoL/ybS', 'cliente', '2026-09-06 23:14:24'),
-(6, 'Administrador', 'admin@farmayopin.com', '$2y$10$u9XGzSaZcvlHPbfGnCNBte4csaG1EvQTqLOOw2zea6dwMcHj3.Sd6', 'admin', '2026-09-07 23:43:33');
+(1, 'administrador1', 'admin@farmayopin.com', '$2y$12$S8fO.V8iMrLswxqo2wSBhuebVyhB/iA3AHd.x0lCF81U1BuNqz936', 'admin', '2026-09-07 23:43:33'),
+(2, 'administrador2', 'admin2@farmayopin.com', '$2y$12$S8fO.V8iMrLswxqo2wSBhuebVyhB/iA3AHd.x0lCF81U1BuNqz936', 'admin', '2026-09-11 20:02:27');
 
 --
 -- Índices para tablas volcadas
@@ -209,19 +191,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `carritos`
 --
 ALTER TABLE `carritos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `compra_items`
 --
 ALTER TABLE `compra_items`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `medios_pago`
@@ -233,13 +215,13 @@ ALTER TABLE `medios_pago`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
